6b92796eb8629956802c449706e07a9e720702e7
root <root@localhost.localdomain>
2017-10-10 10:03:58 -0400

    修改http基础类

--- Information-Gathering/info-scan/lib/connection/http.py
+++ Information-Gathering/info-scan/lib/connection/http.py
@@ -27,29 +27,34 @@ class Request():
 			logger.error(errMsg)
 			raise BloblastDataException(errMsg)
 	
-	def get(self, url = None, lamb = 'GET'):
+	"""def get(self, url = None, lamb = 'GET'):
 		self.__accept(url)
 		try:
 			request = urllib2.Request(url.encode('utf-8'), headers = self.headers)
 			request.get_method = lambda: lamb
 			response = urllib2.urlopen(request, timeout = self.timeout, context = self.context)
-			if response.code == 200:
+			if not cmp(lamb, 'GET') and response.code == 200:
 				logger.info(url + " 200 ok")
-			return response
-		except Exception,e:
+		except urllib2.HTTPError,e:
+			response = e
 			if hasattr(e, 'code'):
 				warnMsg =url + " " + str(e.code) + " failed"
 				logger.warn(warnMsg)
-				code = e.code
 			else:
 				errMsg = str(e) + " " +url
 				logger.error(errMsg)
-			#logger.exception("Exception Logged");
-			return None
+				#logger.exception("Exception Logged");
+		return response"""
 		
-	def post(self, url = None, values = None, lamb = 'POST'):
+	def get(self, url):
+		response = self.connect(url, lamb = 'GET')
+		if response.code == 200:
+			logger.info(url + " 200 ok")
+		return response
+		
+	"""def post(self, url = None, values = None, lamb = 'POST'):
 		self.__accept(url)
-
+		response = None
 		data = None
 		if not cmp(self.headers['Content-Type'], 'application/json'):
 			data = json.dumps(values)
@@ -59,22 +64,64 @@ class Request():
 			request = urllib2.Request(url.encode('utf-8'), data, self.headers)
 			request.get_method = lambda: lamb
 			response = urllib2.urlopen(request, timeout = self.timeout, context = self.context)
-			if response.code == 200:
+			if not cmp(lamb, 'POST') and response.code == 200:
 				logger.info(url + " 200 ok")
-			return response
-		except Exception,e:
+		except urllib2.HTTPError,e:
+			response = e
 			if hasattr(e, 'code'):
 				warnMsg =url + " " + str(e.code) + " failed"
 				logger.warn(warnMsg)
-				code = e.code
 			else:
 				errMsg = str(e) + " " +url
 				logger.error(errMsg)
 			#logger.exception("Exception Logged");
-			return None
+		return response"""
+	def post(self, url, values = None):
+		response = self.connect(url, values = values, lamb = 'POST')
+		if response.code == 200:
+			logger.info(url + " 200 ok")
+		return response
+
+	def connect(self, url, lamb, values = None):
+		self.__accept(url)
+		response = None
+		data = None
+		if not cmp(self.headers['Content-Type'], 'application/json') and values:
+			data = json.dumps(values)
+		elif values:
+			data = urllib.urlencode(values)
+		try:
+			request = urllib2.Request(url.encode('utf-8'), data, self.headers)
+			request.get_method = lambda: lamb
+			response = urllib2.urlopen(request, timeout = self.timeout, context = self.context)
+		except urllib2.HTTPError,e:
+			response = e
+			"""
+			print e.code  
+			print e.reason
+			print e.geturl()  
+			print e.read()
+			"""
+			if hasattr(e, 'code'):
+				warnMsg =url + " " + str(e.code) + " failed"
+				logger.warn(warnMsg)
+			else:
+				errMsg = str(e) + " " +url
+				logger.error(errMsg)
+			#logger.exception("Exception Logged");
+		return response
 
 	def put(self, url, values):
-		return self.post(url, values, 'PUT')
+		response = self.connect(url, values = values, lamb = 'PUT')
+		if response.code == 200:
+			logger.info(url + " 200 ok")
+		return response
+		
+	def delete(self, url, values):
+		response = self.connect(url, lamb = 'DELETE')
+		if response.code == 200:
+			logger.info(url + " 200 ok")
+		return response
 
 	def read(self, response):
 		try:
